3ba161369ce7edc9c1b33106150baa814f40aa0b
sky6776 <137915295+sky6776@users.noreply.github.com>
2023-09-05 09:01:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-
-const checkbox = document.getElementById('checkbox');
+// チェックボックスの要素を取得
+const checkbox1 = document.getElementById('checkbox1');
 const checkbox2 = document.getElementById('checkbox2');
 
-checkbox.checked = localStorage.getItem('checkboxChecked') === 'true';
+// ページが読み込まれたときにチェックボックスの状態をlocalStorageから取得して設定
+checkbox1.checked = localStorage.getItem('checkbox1Checked') === 'true';
 checkbox2.checked = localStorage.getItem('checkbox2Checked') === 'true';
 
-
-checkbox.addEventListener('change', () => {
-    localStorage.setItem('checkboxChecked', checkbox.checked);
+// チェックボックスの状態が変化したときにlocalStorageに保存
+checkbox1.addEventListener('change', () => {
+    localStorage.setItem('checkbox1Checked', checkbox1.checked);
 });
 
 checkbox2.addEventListener('change', () => {
     localStorage.setItem('checkbox2Checked', checkbox2.checked);
 });
 
+// 検索ボックスの要素を取得
 const searchBox = document.getElementById('sbox');
 
-
+// 検索ボタンの要素を取得
 const searchButton = document.getElementById('sbtn');
 
+// チェックボックスを非表示にする関数
 function hideCheckbox(checkboxElement) {
     checkboxElement.parentElement.style.display = 'none';
 }
 
+// チェックボックスを表示する関数
 function showCheckbox(checkboxElement) {
     checkboxElement.parentElement.style.display = 'block';
 }
 
+// 検索ボタンクリック時の処理
 searchButton.addEventListener('click', () => {
-    const keyword = searchBox.value.toLowerCase();
+    const keyword = searchBox.value.toLowerCase(); // 入力されたキーワードを小文字に変換
 
     if (keyword.trim() === '') {
-        showCheckbox(checkbox);
+        // キーワードが空白の場合、すべての項目を表示
+        showCheckbox(checkbox1);
         showCheckbox(checkbox2);
     } else {
-        if (!checkbox.labels[0].textContent.toLowerCase().includes(keyword)) {
-            hideCheckbox(checkbox);
+        // キーワードに一致する項目のみ表示
+        if (!checkbox1.labels[0].textContent.toLowerCase().includes(keyword)) {
+            hideCheckbox(checkbox1);
         } else {
-            showCheckbox(checkbox);
+            showCheckbox(checkbox1);
         }
 
         if (!checkbox2.labels[0].textContent.toLowerCase().includes(keyword)) {
@@ -46,18 +53,4 @@ searchButton.addEventListener('click', () => {
             showCheckbox(checkbox2);
         }
     }
-});
-
-const checkbox = document.getElementById('checkbox');
-const checkbox2 = document.getElementById('checkbox2');
-
-checkbox.checked = localStorage.getItem('checkboxChecked') === 'true';
-checkbox2.checked = localStorage.getItem('checkbox2Checked') === 'true';
-
-checkbox.addEventListener('change', () => {
-    localStorage.setItem('checkboxChecked', checkbox.checked);
-});
-
-checkbox2.addEventListener('change', () => {
-    localStorage.setItem('checkbox2Checked', checkbox2.checked);
 });
